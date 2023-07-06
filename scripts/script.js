50a675d7e2976190e86c414825f91157fd73f176
KillerSosika <skaliush3@gmail.com>
2023-07-06 19:13:06 +0300

    all

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
-let user_even_number=NaN;
+let depositAmount = 100;
+let interestRate = 5;
+let months = 2;
 
-while(isNaN(user_even_number)|| user_even_number%2 !==0 ||user_even_number<10 || user_even_number>50){
-    user_even_number = parseInt(prompt("Введіть парне число в області від 10 до 50:"));
-
-    if(isNaN(user_even_number)){
-        alert("Невірний ввід. Спробуйте ще раз.");
-    }
-}
+let accruedAmount = depositAmount*(1+interestRate/100)**months
+let accruedInterest= (accruedAmount-depositAmount)/depositAmount*100
 
 
-console.log("Половина вашого парного числа", user_even_number/2);
+
+console.log("Сума нарахованих відсотків: " + accruedInterest.toFixed(2));
